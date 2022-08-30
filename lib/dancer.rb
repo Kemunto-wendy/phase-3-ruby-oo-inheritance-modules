691b2e8ba65a8b4290d93c780b0a3cb039d159e7
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-08-30 09:21:23 +0300

    commit tree 3066ed385d2b2388a0f55cab2edbdb999b5812d9

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+  attr_accessor :name
+  def def initialize(name)
+    @name = name
+  end
+
+  class Dancer
+    extend MetaDancing
+  end
+end
